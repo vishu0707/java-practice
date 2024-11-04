8ab416e4872093c9f6b5b8d7046e03ce4ee21bea
vishu0707 <deeptanugade5881@gmail.com>
2024-11-04 13:31:40 +0530

    added comment to the assignment 1

--- src/LetsPrograme/Assign1.java
+++ src/LetsPrograme/Assign1.java
@@ -2,6 +2,7 @@ package LetsPrograme;
 
 public class Assign1 {
     public static void main(String[] args) {
+//        printing this text
         System.out.println("Hey there, I am data!");
     }
 }
