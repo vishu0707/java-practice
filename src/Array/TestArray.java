b8972c5ae54497859b2f33fde7d183637f9d1d25
vishu0707 <deeptanugade5881@gmail.com>
2024-11-04 13:35:54 +0530

    add array demo logic

--- src/Array/TestArray.java
+++ src/Array/TestArray.java
@@ -1,5 +1,10 @@
 package Array;
 
+import java.util.Arrays;
+
 public class TestArray {
-//    adding new array demo
+    public static void main(String[] args) {
+        int[] array = {1, 2, 4, 5};
+        System.out.println("Array = " + Arrays.toString(array));
+    }
 }
